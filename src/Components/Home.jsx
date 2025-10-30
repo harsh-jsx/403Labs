cc3d5fe326826f38e7b2de087e7fda770d0d786d
Harsh <harshh.tsx@gmail.com>
2025-10-30 19:54:02 +0530

    Added vid

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,19 @@
+import React from "react";
+import gsap from "gsap";
+import { useState, useRef, useLayoutEffect } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      className="home-container text-black"
+      style={{ marginTop: "100px", padding: "2rem" }}
+    >
+      <h1 className="home-title text-5xl font-bold font-inter">
+        Brands for the next
+      </h1>
+      <video src="/public/main-bg.mp4" autoPlay muted loop></video>
+    </div>
+  );
 };
 
 export default Home;
